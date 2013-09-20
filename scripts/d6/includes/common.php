1056c3677a79e28ffcba5d150ca4c88bed826011
Onur Rauf Bingol <orbingol@gmail.com>
2013-09-20 08:59:20 +0300

    Added common include files

---
+++ scripts/d6/includes/common.php
@@ -0,0 +1,31 @@
+<?php
+
+// Include needed variables
+require 'variables.php';
+
+// Reads Drupal "sites" directory
+function getSites() {
+
+  global $variables;
+
+  // For compatibility issues...
+  $dir = $variables['dir'];
+
+  // Read directory
+  if($handle = opendir($dir)){
+    while (false !== ($entry = readdir($handle))) {
+        if(is_dir($dir . "/" . $entry) && !is_link($dir . "/" . $entry)) {
+            $entry = trim($entry);
+            $data[] = $entry;
+        }
+     }
+  }
+
+  // Remove standard directories from the array
+  $dirs_after_exclusion = array_diff($data, $variables['excluded_dirs']);
+  $dirs_final = array_values($dirs_after_exclusion);
+
+  // Return Drupal sites
+  return $dirs_final;
+
+}
---
+++ scripts/d6/includes/variables.php
@@ -0,0 +1,7 @@
+<?php
+
+// Define Drupal sites directory
+$variables['dir'] = "/home/user/public_html/sites";
+
+// Set directory exclusion array
+$variables['excluded_dirs'] = array(".", "..", "all", "default");
