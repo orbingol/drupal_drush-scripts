2373e6a52fd11a305134c15ffe6c0bc8b108d42d
Onur Rauf Bingol <orbingol@gmail.com>
2013-09-22 01:54:48 +0300

    Initial commit of watchdog disable/clean script

---
+++ scripts/d6/module_ops/clear_watchdog.php
@@ -0,0 +1,68 @@
+#!/usr/bin/php
+<?php
+
+/**
+ *
+ * Disable "Database Logging" module and empty "watchdog" table
+ *
+ *
+ * Copyright 2012-2013 Onur Rauf Bingol
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Include needed files
+require_once '../includes/variables.php';
+require_once '../includes/common.php';
+
+// Get directories
+$data = getSites();
+
+// Initialize counter variables
+$site_count = count($data);
+$counter = 1;
+
+// Preparing the variables...
+$user_name = 'my_user';
+
+// Preparing Drush commands...
+$remove_user_command = 'drush -y ucan ' . $user_name;
+
+// Start looping within the sites
+foreach ($data as $d) {
+
+  // Set directory path for easy access
+  $dir_path = $variables['dir'] . '/' . $d;
+
+  // Change to the site directory in consideration
+  chdir($dir_path);
+
+  // Print an informational message
+  echo 'Site #: ' . $counter . ' / ' . $site_count . "\n" . 'Processing: ' . $d . "\n";
+
+  // Disable "Database Logging"
+  system('drush -y pm-disable dblog');
+
+  // Empty "watchdog" table
+  system('drush sql-query "TRUNCATE watchdog"');
+
+  // Clear all caches
+  system('drush cc all');
+
+  // Increase counter
+  $counter++;
+
+}
+
+?>
