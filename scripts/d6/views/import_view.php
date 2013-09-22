c83f40856a6bb04140d1f865af9bdb77bba9ba41
Onur Rauf Bingol <orbingol@gmail.com>
2013-09-22 18:36:47 +0300

    Initial commit of views import script

---
+++ scripts/d6/views/import_view.php
@@ -0,0 +1,59 @@
+#!/usr/bin/php
+<?php
+
+/**
+ *
+ * Import a view
+ *
+ *
+ * Copyright 2012-2013 Onur Rauf Bingol
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Include needed files
+require_once '../includes/variables.php';
+require_once '../includes/common.php';
+
+// Get directories
+$data = getSites();
+
+// Initialize counter variables
+$site_count = count($data);
+$counter = 1;
+
+// Start looping within the sites
+foreach ($data as $d) {
+
+  // Set directory path for easy access
+  $dir_path = $variables['dir'] . '/' . $d;
+
+  // Change to the site directory in consideration
+  chdir($dir_path);
+
+  // Print some information
+  echo 'Site #: ' . $counter . ' / ' . $site_count . "\n" . 'Processing: ' . $d . "\n";
+
+  // Import the view (it is not included in this package)
+  system('drush views-import drush-views/calendar.view');
+
+  // Clear all caches
+  system('drush cc all');
+
+  // Increase counter
+  $counter++;
+
+}
+
+?>
